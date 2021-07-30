8ea42c7266987f31776f3a769282a5836a97fe71
Aleksan <Dev.aleksan@gmail.com>
2021-07-30 03:03:34 +0300

    feat: add timer cleanup after modal opens

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,4 +1,5 @@
 const modals = () => {
+    let modalTimer;
 
     function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 
@@ -18,6 +19,9 @@ const modals = () => {
                 });
                 modal.style.display = 'block';
                 document.body.classList.add('modal-open');
+                if (modalTimer) {
+                    clearInterval(modalTimer);
+                }
             });
         });
 
@@ -41,10 +45,9 @@ const modals = () => {
     }
 
     function showModalByTime(selector, time) {
-        setTimeout(function () {
+        modalTimer = setTimeout(function () {
             document.querySelector(selector).style.display = 'block';
             document.body.classList.add('modal-open');
-
         }, time);
     }
 
@@ -53,7 +56,7 @@ const modals = () => {
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close', false);
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
-    showModalByTime('.popup', 60000);
+    showModalByTime('.popup_engineer', 60000);
 };
 
 export default modals;
